c404a57cb85c872fb444e437eda1efbfeb59c12e
davit <davit@GOSH>
2024-09-24 13:04:11 +0400

    Add project files.

---
+++ ALeapYear/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Sheiyvanet weli nakianobis gasagebad:");
+
+
+            int Weli = int.Parse(Console.ReadLine());
+
+if (Weli % 4 == 0)
+{
+    Console.WriteLine($"{Weli} aris nakiani weli");
+}
+else if (Weli % 100 != 0)
+{
+    Console.WriteLine($"{Weli} ar aris nakiani weli");
+}
+else if (Weli % 400 != 0)
+{
+    Console.WriteLine($"{Weli} ar aris nakiani weli");
+}
+else if (Weli % 4 != 0)
+{
+    Console.WriteLine($"{Weli} ar aris nakiani weli");
+}
+else
+{
+    Console.WriteLine("Araswori tarigi");
+}
+   
+
+        Console.ReadLine();
